22310b0596998322aee1defdd0454ed42a9504e9
Chiranjeevi Gurram <chiranjeevigurram@Chiranjeevis-MacBook-Pro.local>
2026-05-21 15:29:11 +0530

    [chiru] : adds decrementing logic when rooms are booked

--- src/main/java/com/hotel/book/mongodbServiceImp/HotelServiceImp.java
+++ src/main/java/com/hotel/book/mongodbServiceImp/HotelServiceImp.java
@@ -3,22 +3,31 @@ package com.hotel.book.mongodbServiceImp;
 import com.hotel.book.model.Booking;
 import com.hotel.book.model.Hotel;
 import com.hotel.book.repository.BookingRepository;
-import com.hotel.book.repository.hotelrepository;
+import com.hotel.book.repository.Hotelrepository;
+import com.hotel.book.repository.Hotelrepository;
 import com.hotel.book.requestDTO.BookHotelRequestDTO;
 import com.hotel.book.responseDTO.SearchHotelResponse;
 import com.hotel.book.service.HotelService;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
+
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+
 
 import java.util.List;
 
 @Service
 public class HotelServiceImp implements HotelService {
-    private final hotelrepository hotelRepository;
+    private final Hotelrepository hotelRepository;
     private  final BookingRepository bookingRepository;
-    public HotelServiceImp(hotelrepository hotelRepository, BookingRepository bookingRepository) {
+    private final MongoTemplate mongoTemplate;
+    public HotelServiceImp(Hotelrepository hotelRepository, BookingRepository bookingRepository, MongoTemplate mongoTemplate) {
         this.hotelRepository = hotelRepository;
 
         this.bookingRepository = bookingRepository;
+        this.mongoTemplate = mongoTemplate;
     }
 
     @Override
@@ -35,8 +44,10 @@ public class HotelServiceImp implements HotelService {
     public void bookRooms(String userID, BookHotelRequestDTO bookHotelRequestDTO) throws  RuntimeException{
 
         Hotel hotel = hotelRepository.getHotelById(bookHotelRequestDTO.hotel_id());
+
+        updateRoomsAvailable(bookHotelRequestDTO);
+
         Boolean areRoomsAvailable = hotel.roomsBooked() < hotel.totalRooms();
-//        Boolean areRoomsAvailable = true;
         if(areRoomsAvailable) {
             Booking booking = new Booking(null, userID, bookHotelRequestDTO.hotel_id(), bookHotelRequestDTO.rooms());
             bookingRepository.save(booking);
@@ -44,6 +55,13 @@ public class HotelServiceImp implements HotelService {
         }
 
         throw  new RuntimeException("no rooms left in this hotel");
+    }
+
+    private void updateRoomsAvailable(BookHotelRequestDTO bookHotelRequestDTO) {
+        Query query = new Query(Criteria.where("id").is(bookHotelRequestDTO.hotel_id()));
+        Update update = new Update().inc("roomsBooked", -bookHotelRequestDTO.rooms());
+
+        mongoTemplate.updateFirst(query, update, Hotel.class);
     }
 
 }
